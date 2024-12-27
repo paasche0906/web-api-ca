2f988775fe67cffbfb8af3bf96590ea981af81b3
unknown <jiahchengpan@gmail.com>
2024-12-27 12:19:19 +0000

    Beautify the login page.

--- react-movies/src/pages/loginPage.js
+++ react-movies/src/pages/loginPage.js
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { AuthContext } from '../contexts/authContext';
+import { AuthContext } from "../contexts/authContext";
 import { Link } from "react-router-dom";
+import '../css/LoginPage.css';
 
-const LoginPage = props => {
+const LoginPage = () => {
     const context = useContext(AuthContext);
 
     const [userName, setUserName] = useState("");
@@ -22,20 +23,37 @@ const LoginPage = props => {
     }
 
     return (
-        <>
-            <h2>Login page</h2>
-            <p>You must log in to view the protected pages </p>
-            <input id="username" placeholder="user name" onChange={e => {
-                setUserName(e.target.value);
-            }}></input><br />
-            <input id="password" type="password" placeholder="password" onChange={e => {
-                setPassword(e.target.value);
-            }}></input><br />
-            {/* Login web form  */}
-            <button onClick={login}>Log in</button>
-            <p>Not Registered?
-                <Link to="/signup">Sign Up!</Link></p>
-        </>
+        <div className="login-container">
+            <div className="login-form">
+                <h2 className="login-title">Login to your account</h2>
+                <p className="login-subtitle">
+                    In order to use the editing and rating capabilities of TMDB, as well as get personal recommendations, you will need to log in to your account. If you do not have an account, registering for an account is free and simple. <Link to="/signup" className="login-link">Click here</Link> to get started.
+                </p>
+                <label htmlFor="username" className="login-label">Username:</label>
+                <input
+                    className="login-input"
+                    id="username"
+                    placeholder="Enter your username"
+                    onChange={(e) => {
+                        setUserName(e.target.value);
+                    }}
+                />
+                <label htmlFor="password" className="login-label">Password:</label>
+                <input
+                    className="login-input"
+                    id="password"
+                    type="password"
+                    placeholder="Enter your password"
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                <button className="login-button" onClick={login}>Login</button>
+                <p className="login-footer">
+                    <Link to="/reset-password" className="login-link">Reset password</Link>
+                </p>
+            </div>
+        </div>
     );
 };
 
